799a3c0f1539f3a4a7bbbec36c9afc96bf80eecd
srikrishna <srikrishna>
2014-01-26 22:22:41 -0800

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  #Returns the Gravatar for the given user
+  def gravatar_for(user)
+    gravatar_url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSh2Blid-YJF50VVZ_BBVlQXRhXnTBMPEmJ0yCKldt3p7QekW4SmA"
+    #gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    #gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
