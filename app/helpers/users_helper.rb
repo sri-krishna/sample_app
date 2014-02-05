4bfc540bbc709404a893909b4fe196d17c17d1c1
srikrishna <srikrishna>
2014-02-04 23:56:11 -0800

    Finish user edit, update, index, and destroy action

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   #Returns the Gravatar for the given user
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50} )
     gravatar_url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSh2Blid-YJF50VVZ_BBVlQXRhXnTBMPEmJ0yCKldt3p7QekW4SmA"
     #gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    #gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+    #gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
